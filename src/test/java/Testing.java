8f881ba75e1a4cbe8578ba18e5da815adfb7dbf0
Chandru <chandruvijai1140@gmail.com>
2022-07-11 16:04:08 +0530

    first commit rs demo

---
+++ src/test/java/Testing.java
@@ -0,0 +1,13 @@
+public class Testing {
+
+    public static void main (String[] args){
+        System.out.println("one");
+        System.out.println("two");
+        System.out.println("three");
+        System.out.println("four");
+        System.out.println("five");
+        System.out.println("six");
+        System.out.println("seven");
+
+    }
+}
